1827e8e80a6fd2f03ee379cb4397e52678820200
Tonio Serna <antonio.serna.gutierrez@joincoup.com>
2017-05-28 12:49:09 +0200

    Validate presence

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+  validates :post_id, :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validate :title, :hashtags, :body, :reading_time, presence: true
 end
