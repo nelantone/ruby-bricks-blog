f351a45f039264324bcd54d7aa2adbb002712692
Tonio Serna <antonio.serna.gutierrez@joincoup.com>
2017-05-30 10:15:36 +0200

    Add first tests & rubocop gems

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'fuubar'
+  gem 'rubocop'
+  gem 'rubocop-rspec'
 end
 
 group :development do
---
+++ spec/model/post_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string:true
+
+require 'rails_helper'
+
+RSpec.describe Post, type: :model  do 
+  it 'has a valid post' do
+    post = FactoryGirl.build(:post)
+    expect(post).to be_valid
+  end
+
+  it 'is invalid without body' do
+    invalid_post = FactoryGirl.build(:post, body: nil)
+    expect(invalid_post).not_to be_valid
+  end
+end
