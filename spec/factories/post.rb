f2e2c28e122bc5547577cbc3d73d9cc3f25a76be
Tonio Serna <antonio.serna.gutierrez@joincoup.com>
2017-07-09 02:30:50 +0200

    Refactor front-end based on new gems

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :post do
     title { Faker::Name.title }
     hashtags { Faker::StarWars.planet }
-    body { Faker::StarWars.wookie_sentence  }
+    body { Faker::StarWars.wookie_sentence }
     reading_time { 1.minute }
   end
 end
