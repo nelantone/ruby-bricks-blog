757bf14463bbfcf4b871a0969eb7ee0e196b29ab
Tonio Serna <antonio.serna.gutierrez@joincoup.com>
2017-07-09 02:33:55 +0200

    Add: root index#post route, ckeditor_assets migration, lib/template forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528100536) do
+ActiveRecord::Schema.define(version: 20170708195718) do
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
